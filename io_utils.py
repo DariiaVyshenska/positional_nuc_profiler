7f5576e207cc05f640524e7ac2334519b5aac69a
Dariia Vyshenska <dariia.vyshenska@gmail.com>
2025-01-31 09:22:47 -0800

    reafactor: refactoring io_utils functions

--- io_utils.py
+++ io_utils.py
@@ -1,7 +1,7 @@
 import pysam
 import pandas as pd
 import os
-import sys
+from contextlib import contextmanager
 from exceptions import RefError
 
 def get_output_file_path(output_file_path, bam_file_path, nt_positions):
@@ -12,25 +12,32 @@ def get_output_file_path(output_file_path, bam_file_path, nt_positions):
   
   bam_file_basename = os.path.basename(bam_file_path)
   smpl_id = bam_file_basename.removesuffix('.fastq.gz.bam').removesuffix('.bam')
-  positions_str = '-'.join([str(pos) for pos in nt_positions])
+  positions_str = '-'.join(map(str, nt_positions))
   return f"{output_file_path}/{smpl_id}_{positions_str}_complex_freqs.csv"
 
 def codon_stats_to_csv(codon_stats, output_path):
   df = pd.DataFrame(codon_stats, columns=['CODON', 'FREQUENCY', 'DEPTH'])
-  df = df.sort_values(by='FREQUENCY', ascending=False)
+  df.sort_values(by='FREQUENCY', ascending=False, inplace=True)
   print("\nSelected nucleotide combinations (final results):")
   print(df)
   df.to_csv(output_path, index=False)
-
-def validate_bam_index(bam_file):
-    bai_file = bam_file + ".bai"
-    csi_file = bam_file + ".csi"
-    if not any(os.path.exists(file) for file in [bai_file, csi_file]):
-      raise RefError(f'RefError: No index file found for {bam_file!r}. Please index the BAM file before proceeding.')
-    
+  
+@contextmanager
 def open_and_validate_bam(bam_file):
-  bam = pysam.AlignmentFile(bam_file, "rb")
-  validate_bam_index(bam_file)
-  if not bam.lengths or len(bam.references) != 1:
-    raise RefError('RefError: input .bam must contain one and only one reference (chromosome)')
-  return bam
+  try:
+    with pysam.AlignmentFile(bam_file, "rb") as bam:
+      try:
+        bam.check_index()
+      except ValueError as e:
+        raise RefError(
+          f"RefError: No index file found for {bam_file!r}. Please index the BAM file "
+          f"before proceeding. Error: {e}"
+        )
+        
+      if not bam.lengths or len(bam.references) != 1:
+        raise RefError('RefError: input .bam must contain one and only one reference (chromosome)')
+    
+      yield bam
+
+  except (FileNotFoundError, OSError, pysam.utils.SamtoolsError) as e:
+    raise RefError(f'RefError: Failed to open BAM file: {bam_file!r}. Error: {e}')
